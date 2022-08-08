6ed4fd6b2dd8ca38729c37c6ecc44f3a5169a8b2
Amaury Diaz <amauryuh@gmail.com>
2022-08-08 22:49:04 +0200

    add link to explorer.

--- src/layouts/dapp/components/AssetCard.tsx
+++ src/layouts/dapp/components/AssetCard.tsx
@@ -14,6 +14,7 @@ import { decodeHex } from 'orbyc-core/utils/encoding';
 import { DataSourceContext } from 'providers/blockchain/provider';
 import { useCallback } from 'react';
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 // const IMAGE =
 //   'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80';
@@ -93,46 +94,48 @@ export default function AssetCard(props: AssetCardProps) {
   );
 
   return (
-    <Center py={12}>
-      <Box
-        cursor={`pointer`}
-        role={'group'}
-        p={6}
-        maxW={'330px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'lg'}
-        pos={'relative'}
-        zIndex={1}
-      >
+    <Link to={`/dapp/${data?.asset.getId()}`}>
+      <Center py={12}>
         <Box
+          cursor={`pointer`}
+          role={'group'}
+          p={6}
+          maxW={'330px'}
+          w={'full'}
+          bg={useColorModeValue('white', 'gray.800')}
+          boxShadow={'2xl'}
           rounded={'lg'}
-          mt={-12}
           pos={'relative'}
-          height={'230px'}
-          _after={{
-            transition: 'all .3s ease',
-            content: '""',
-            w: 'full',
-            h: 'full',
-            pos: 'absolute',
-            top: 5,
-            left: 0,
-            // backgroundImage: `url(${data?.metadata.getBackground()?.getAttachment()})`,
-            filter: 'blur(15px)',
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: 'blur(20px)',
-            },
-          }}
+          zIndex={1}
         >
-          {assetImage}
+          <Box
+            rounded={'lg'}
+            mt={-12}
+            pos={'relative'}
+            height={'230px'}
+            _after={{
+              transition: 'all .3s ease',
+              content: '""',
+              w: 'full',
+              h: 'full',
+              pos: 'absolute',
+              top: 5,
+              left: 0,
+              // backgroundImage: `url(${data?.metadata.getBackground()?.getAttachment()})`,
+              filter: 'blur(15px)',
+              zIndex: -1,
+            }}
+            _groupHover={{
+              _after: {
+                filter: 'blur(20px)',
+              },
+            }}
+          >
+            {assetImage}
+          </Box>
+          {assetContent}
         </Box>
-        {assetContent}
-      </Box>
-    </Center>
+      </Center>
+    </Link>
   );
 }
